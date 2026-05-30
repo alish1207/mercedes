44adaf7b58b68be7bd7d5379a3adc35ba6ab3ecc
alish1207 <khairullaevalisher07@gmail.com>
2026-05-30 18:14:32 +0500

    ejhvbsdhbv

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,19 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+const buyProduct = async (product) => {
+  const tg = window.Telegram.WebApp;
+
+  await fetch("https://mercedes-sigma-coral.vercel.app/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userId: tg.initDataUnsafe.user.id,
+      name: product.name,
+      price: product.price,
+    }),
+  });
+};
